d63de57fad2d37186f7bfc1369b1a83bf818d390
Cecilia <ceciferreri@gmail.com>
2022-06-14 20:49:36 -0300

    añadido desafio arrays

--- JS/main.js
+++ JS/main.js
@@ -1,56 +1,26 @@
-//Seleccione su país
-
-alert("Seleccione un país de la siguiente lista: \n Argentina \n Chile \n México \n Canadá \n Estados Unidos ")
-
-let escribirPais = prompt("Escriba el país seleccionado")
-
-function elijaPais(pais){
-    
-    switch (pais) {
-        case "Argentina":
-            return "seleccionó Argentina";
-        case "Chile":
-            return "seleccionó Chile"
-        case "México":
-            return "Seleccionó México"
-        case "Canadá":
-            return "Seleccionó Canadá"
-        case "Estados Unidos":
-            return "Seleccionó Estados Unidos"
-        default:
-            return "Error. El país seleccionado no se encuentra en las opciones"
-    }
+//Carrito con array y prompt
+let entrada = prompt("Ingresar producto. \n Para continuar, escriba CONTINUAR");
+const compras = [];
+while (entrada != "CONTINUAR"){
+    compras.push(entrada);
+    entrada = prompt("Ingresar producto \n Para continuar, escriba CONTINUAR")
 }
-let resultado = elijaPais(escribirPais);
-alert(resultado)
 
+console.log(compras);
 
+// Carrito con objeto - funcion constructora
+let ingProducto = prompt("Ingrese producto")
+let ingNombre = prompt("Ingrese su nombre")
+let ingDireccion = prompt("Ingrese su direccion")
+let ingTelefono = prompt("Ingrese su telefono")
 
-//Nombre de usuario y contraseña
-
-function usuario(){
-    while (nombreUsuario == "" ) {
-        alert ("Nombre ingresado no es valido");
-        nombreUsuario = prompt("Ingrese nombre de usuario")
-    } 
+function RealizarPedido (producto, nombre, direccion, telefono){
+    this.producto = producto;
+    this.nombre = nombre,
+    this.direccion = direccion,
+    this.telefono = telefono;
 }
- 
-function contrasenia(){
-    while (ingreseContrasenia == "" ) {
-        alert ("Contraseña ingresada no es valida");
-        ingreseContrasenia = prompt("Ingrese contraseña")
-    }
-}
- 
-let nombreUsuario = prompt("Ingrese nombre de usuario")
-usuario()
-let ingreseContrasenia = prompt("Ingrese contraseña")
-contrasenia()
- 
- 
- 
-if (nombreUsuario != "" && contrasenia != ""){
-    alert ("Datos ingresados correctamente")
-} else {
-    alert ("Los datos ingresados son invalidos")
-}
+
+const uno = new RealizarPedido(ingProducto, ingNombre, ingDireccion, ingTelefono)
+
+console.log(uno);
